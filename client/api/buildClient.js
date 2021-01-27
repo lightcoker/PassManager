5f037e15fe505c30e938bdd2fab56360a06ab3c9
Albert Lin <lightcoker@gapp.nthu.edu.tw>
2021-01-27 21:32:57 +0800

    fix(client): Fix client base url

--- client/api/buildClient.js
+++ client/api/buildClient.js
@@ -4,7 +4,7 @@ export default ({ req }) => {
   if (typeof window === "undefined") {
     // executed on server
     return axios.create({
-      baseURL: process.env.HOSTING_SITE,
+      baseURL: "http://www.albertapp.codes", // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
